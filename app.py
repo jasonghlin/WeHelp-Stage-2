84836494da570a393eaedef960287dfa3ab888b0
jasonghlin <m338107001@tmu.edu.tw>
2024-07-12 20:08:29 +0800

    fix static file logic

--- app.py
+++ app.py
@@ -36,19 +36,19 @@ app.add_middleware(
 # Static Pages (Never Modify Code in this Block)
 @app.get("/", include_in_schema=False)
 async def index(request: Request):
-	return serve_static_page("./static/index.html", media_type="text/html")
+	return serve_static_page("index.html")
 @app.get("/attraction/{id}", include_in_schema=False)
 async def attraction(request: Request, id: int):
-	return serve_static_page("./static/attraction.html", media_type="text/html")
+	return serve_static_page("attraction.html")
 @app.get("/booking", include_in_schema=False)
 async def booking(request: Request):
-	return serve_static_page("./static/booking.html", media_type="text/html")
+	return serve_static_page("booking.html")
 @app.get("/thankyou", include_in_schema=False)
 async def thankyou(request: Request):
-	return serve_static_page("./static/thankyou.html", media_type="text/html")
+	return serve_static_page("thankyou.html")
 @app.get("/memberpage", include_in_schema=False)
 async def thankyou(request: Request):
-	return serve_static_page("./static/member page.html", media_type="text/html")
+	return serve_static_page("member page.html")
 # -----------------------------------------------
 
 
